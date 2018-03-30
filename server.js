8fb4f128f1c9c363e66ac427e855b409e5e3d2e0
Eliska <eliska.achee@gmail.com>
2018-03-30 12:49:53 -0600

    fixed insert and printed professor

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.set('port', PORT)
 
 function handleAddComment(req, res) {
   console.log("Handling Add Comment");
+  
   var client = new pg.Client(connectionString);
   client.connect(function(error) {
     if(error) {
@@ -27,13 +28,23 @@ function handleAddComment(req, res) {
       res.status(500);
       res.json({success: false});
     }
-    console.log("Stuff: " + req.body.comment_text + req.body.student_id + req.body.course_id + req.body.professor_id);
-    var sql = "INSERT INTO comment(comment_text, student_id, course_id, professor_id) VALUES($1::text, $2::int, $3::int, $4::int) returning id";
-    var parameters = [req.body.comment_text, req.body.student_id, req.body.course_id, req.body.professor_id];
+    if(req.body.professor_id) {
+      console.log(req.body);
+      // console.log("Stuff: " + req.body.comment + req.body.student_id + req.body.course_id + req.body.professor_id);
+      var sql = "INSERT INTO comment(comment_text, student_id, course_id, professor_id) VALUES($1::text, $2::int, $3::int, $4::int) returning id";
+      var parameters = [req.body.comment, 1, req.body.coursecode, req.body.professor_id];//req.body.student_id, req.body.course_id, req.body.professor_id];
+    } else {
+      console.log(req.body);
+      // console.log("Stuff: " + req.body.comment + req.body.student_id + req.body.course_id);
+      var sql = "INSERT INTO comment(comment_text, student_id, course_id) VALUES($1::text, $2::int, $3::int) returning id";
+      var parameters = [req.body.comment, 1, req.body.coursecode];//req.body.student_id, req.body.course_id, req.body.professor_id];
+    }
     var query = client.query(sql, parameters, function(error, result) {
       client.end(function(error) {
         if(error) {
           throw error;
+        } else {
+          console.log ("Insert complete");
         }
       });
 
@@ -43,7 +54,7 @@ function handleAddComment(req, res) {
         res.json({success: false});
       }
       res.status(200);
-      res.json(result);
+      res.redirect('/homepage'); //json(result);
     });
   });
 }
@@ -61,7 +72,6 @@ function handleGetClassComments(req, res) {
   });
 }
 
-
 function handleGetClassList(req, res) {
   getClassListFromDB(function(error, result) {
     if(error || result == null) {
@@ -69,12 +79,14 @@ function handleGetClassList(req, res) {
       res.json({success: false});
     } else {
       res.status(200);
-      var html = "<select id=\"coursecode\">";
+      var html = "<select name=\"coursecode\" id=\"coursecode\">";
       var course;
       for(course in result) {
-        html += "<option value=\"" + result[course].course_code + "\">" + result[course].course_code + "</option>";
+        // html += "<option name=\"" + result[course].id + "\" value=\"" + result[course].course_code + "\">" + result[course].course_code + "</option>";
+        html += "<option value=\"" + result[course].id + "\">" + result[course].course_code + "</option>";
       }
       html += "</select>";
+      // console.log(html);
       var params = {result: new Buffer(html)};
       res.set('Content-Type', 'text/html');
       res.render('homepage', params);
@@ -83,6 +95,7 @@ function handleGetClassList(req, res) {
 }
 
 function getClassCommentsFromDB(classcode, callback) {
+  console.log("Class code I got: " + classcode);
   var client = new pg.Client(connectionString);
   client.connect(function(error) {
     if(error) {
@@ -90,7 +103,7 @@ function getClassCommentsFromDB(classcode, callback) {
       callback(error, null);
     }
 
-    var sql = "SELECT comment_text, date_added, username, course_code, p.first_name, p.last_name FROM comment c JOIN student s ON c.student_id = s.id JOIN professor p ON c.professor_id = p.id JOIN course co ON c.course_id = co.id WHERE course_code = $1::text";
+    var sql = "SELECT comment_text, date_added, username, course_code, p.first_name, p.last_name FROM comment c JOIN student s ON c.student_id = s.id JOIN course co ON c.course_id = co.id LEFT JOIN professor p ON c.professor_id = p.id WHERE co.id = $1::int";
     var parameters = [classcode];
     var query = client.query(sql, parameters, function(error, result) {
       client.end(function(error) {
@@ -115,7 +128,6 @@ function getClassListFromDB(callback) {
       console.log("ERROR: Could not connect to database: " + error);
       callback(error, null);
     }
-
     var sql = "SELECT * FROM course";
     var query = client.query(sql, function(error, result) {
       client.end(function(error) {
@@ -123,7 +135,6 @@ function getClassListFromDB(callback) {
           throw error;
         }
       });
-
       if(error) {
         console.log("Error in query: " + error);
         callback(error, null);
